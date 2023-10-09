3e259a871a71d5f4a39b849200c38baf45debc51
Jack Werner <its.jack.werner@gmail.com>
2023-10-09 16:38:26 -0400

    added variables for the secret keys

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+  type = string
+  sensitive = true
+}
 
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type = string
+  sensitive = true
+}
 
 variable "region" {
   description = "AWS region"
