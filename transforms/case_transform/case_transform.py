4e0de03f6fa8f5acc7abc46c976606e778e58fff
Jack Werner <its.jack.werner@gmail.com>
2023-10-09 12:01:34 -0400

    fixed typos in json config. also fixed types for the parameters. Changed case_transform to get df from self.toDF

---
+++ transforms/case_transform/case_transform.py
@@ -0,0 +1,29 @@
+from awsglue import DynamicFrame
+import pyspark.sql.functions as F
+from pyspark.sql.utils import AnalysisException
+
+def case_transform(self, column_name, case):
+    """
+    case_transform will take your specified column name and either transform it to all upper or all lowercase.
+
+    params:
+        co 
+    """
+    df = self.toDF()
+
+    try:
+        if df.schema[column_name].dataType.simpleString() == "string":
+            if case == "uppercase":
+                df = df.withColumn(column_name, F.upper(F.col(column_name)))
+            else:
+                df = df.withColumn(column_name, F.lower(F.col(column_name)))
+        else:
+            raise AnalysisException(f"Column '{column_name}' is not of StringType.")
+    except KeyError:
+        raise ValueError(f"Column '{column_name}' not found in DataFrame.")
+    
+    return DynamicFrame.fromDF(df, self.glue_ctx, "case_transform")    
+    
+    
+    
+DynamicFrame.case_transform = case_transform
