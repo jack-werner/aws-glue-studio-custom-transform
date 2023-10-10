9172b9afefeeeffa735ea36e60f45f759632c9ff
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 12:30:59 -0400

    added back example s3 object

--- tf_modules/custom_glue_studio_transform/main.tf
+++ tf_modules/custom_glue_studio_transform/main.tf
@@ -2,20 +2,28 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_object" "json" {
-  bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
-  key    = "${var.filename}.json"
-  source = "${var.local_path}/${var.filename}.json"
-  server_side_encryption = "aws:kms"
+# resource "aws_s3_object" "json" {
+#   bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
+#   key    = "${var.filename}.json"
+#   source = "${var.local_path}/${var.filename}.json"
+#   server_side_encryption = "aws:kms"
 
-  # etag = filemd5("${var.local_path}/${var.filename}.json")
-}
+#   # etag = filemd5("${var.local_path}/${var.filename}.json")
+# }
 
-resource "aws_s3_object" "python" {
-  bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
-  key    = "${var.filename}.py"
-  source = "${var.local_path}/${var.filename}.py"
-  server_side_encryption = "aws:kms"
+# resource "aws_s3_object" "python" {
+#   bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
+#   key    = "${var.filename}.py"
+#   source = "${var.local_path}/${var.filename}.py"
+#   server_side_encryption = "aws:kms"
 
-  # etag = filemd5("${var.local_path}/${var.filename}.py")
+#   # etag = filemd5("${var.local_path}/${var.filename}.py")
+# }
+
+resource "aws_s3_object" "object" {
+  bucket = var.s3_bucket
+  key = "target/example.json"
+  source = "transforms/case_transform/case_transform.json"
+
+  etag = filemd5("transforms/case_transform/case_transform.json")
 }
