2c64df78549f9538a6efd5c98672a167de6a464a
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 12:45:15 -0400

    fixed bucket and object path in module. removed encryption.

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-# module "custom_glue_studio_case_transform" {
-#   source     = "./tf_modules/custom_glue_studio_transform/"
-#   region     = "us-east-1"
-#   account_id = var.AWS_ACCOUNT_ID
-#   filename   = "case_transform_2"
-#   local_path = "transforms/case_transform"
-# }
-
-resource "aws_s3_object" "object" {
-  bucket = "aws-glue-assets-${var.AWS_ACCOUNT_ID}-${var.region}"
-  key = "transforms/target/example.json"
-  source = "transforms/case_transform/case_transform_2.json"
-
-  etag = filemd5("transforms/case_transform/case_transform_2.json")
+module "custom_glue_studio_case_transform" {
+  source     = "./tf_modules/custom_glue_studio_transform/"
+  region     = "us-east-1"
+  account_id = var.AWS_ACCOUNT_ID
+  filename   = "case_transform_2"
+  local_path = "transforms/case_transform"
 }
+
+# resource "aws_s3_object" "object" {
+#   bucket = "aws-glue-assets-${var.AWS_ACCOUNT_ID}-${var.region}"
+#   key = "transforms/target/example.json"
+#   source = "transforms/case_transform/case_transform_2.json"
+
+#   etag = filemd5("transforms/case_transform/case_transform_2.json")
+# }
