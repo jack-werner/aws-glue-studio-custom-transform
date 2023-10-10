930b02be1ad2402a325899948a64f3e24dec369e
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 11:13:33 -0400

    renamed case_transform files to case_transform_2 so that the new transform will be visible in glue studio

--- main.tf
+++ main.tf
@@ -6,5 +6,6 @@ module "custom_glue_studio_case_transform" {
   source = "tf_modules/custom_glue_studio_transform/"
   region = "us-east-1"
   account_id = var.account_id
-  
+  file_name = "case_transform_2"
+  local_path = "transforms/case_transform"
 }
