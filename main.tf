1d603b19f7941c6b3e2cd9426c661dc2c0c83230
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 12:34:43 -0400

    added back the s3_bucket param and put the test object back in the root main file

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-module "custom_glue_studio_case_transform" {
-  source     = "./tf_modules/custom_glue_studio_transform/"
-  region     = "us-east-1"
-  account_id = var.AWS_ACCOUNT_ID
-  filename   = "case_transform_2"
-  local_path = "transforms/case_transform"
+# module "custom_glue_studio_case_transform" {
+#   source     = "./tf_modules/custom_glue_studio_transform/"
+#   region     = "us-east-1"
+#   account_id = var.AWS_ACCOUNT_ID
+#   filename   = "case_transform_2"
+#   local_path = "transforms/case_transform"
+# }
+
+resource "aws_s3_object" "object" {
+  bucket = var.s3_bucket
+  key = "target/example.json"
+  source = "transforms/case_transform/case_transform.json"
+
+  etag = filemd5("transforms/case_transform/case_transform.json")
 }
