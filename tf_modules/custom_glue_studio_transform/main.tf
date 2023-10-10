9ce40609be3567b8f878fdf2f3cd25fe1423031e
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 12:55:40 -0400

    fixed paths for the target upload. removed commented out code

--- tf_modules/custom_glue_studio_transform/main.tf
+++ tf_modules/custom_glue_studio_transform/main.tf
@@ -3,19 +3,17 @@ provider "aws" {
 }
 
 resource "aws_s3_object" "json" {
-  bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
-  key    = "${var.filename}.json"
+  bucket = "aws-glue-assets-${var.account_id}-${var.region}"
+  key    = "transforms/${var.filename}.json"
   source = "${var.local_path}/${var.filename}.json"
-  server_side_encryption = "aws:kms"
 
   etag = filemd5("${var.local_path}/${var.filename}.json")
 }
 
 resource "aws_s3_object" "python" {
-  bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
-  key    = "${var.filename}.py"
+  bucket = "aws-glue-assets-${var.account_id}-${var.region}"
+  key    = "transforms/${var.filename}.py"
   source = "${var.local_path}/${var.filename}.py"
-  server_side_encryption = "aws:kms"
 
   etag = filemd5("${var.local_path}/${var.filename}.py")
 }
