3578f97d5443ee8b363e9b5c1114299b1972a3d0
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 11:59:50 -0400

    added data to kms key references

--- tf_modules/custom_glue_studio_transform/main.tf
+++ tf_modules/custom_glue_studio_transform/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_object" "json" {
   bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
   key    = "${var.filename}.json"
   source = "${var.local_path}/${var.filename}.json"
-  kms_key_id = aws_kms_key.this.arn
+  kms_key_id = data.aws_kms_key.this.arn
 
   etag = filemd5("${var.local_path}/${var.filename}.json")
 }
@@ -19,7 +19,7 @@ resource "aws_s3_object" "python" {
   bucket = "aws-glue-assets-${var.account_id}-${var.region}/transforms"
   key    = "${var.filename}.py"
   source = "${var.local_path}/${var.filename}.py"
-  kms_key_id = aws_kms_key.this.arn
+  kms_key_id = data.aws_kms_key.this.arn
 
   etag = filemd5("${var.local_path}/${var.filename}.py")
 }
