d63bf094a8045db095a6d7a2f468d00646cae90d
Jack Werner <its.jack.werner@gmail.com>
2023-10-10 12:46:09 -0400

    removed example object

--- main.tf
+++ main.tf
@@ -9,11 +9,3 @@ module "custom_glue_studio_case_transform" {
   filename   = "case_transform_2"
   local_path = "transforms/case_transform"
 }
-
-# resource "aws_s3_object" "object" {
-#   bucket = "aws-glue-assets-${var.AWS_ACCOUNT_ID}-${var.region}"
-#   key = "transforms/target/example.json"
-#   source = "transforms/case_transform/case_transform_2.json"
-
-#   etag = filemd5("transforms/case_transform/case_transform_2.json")
-# }
