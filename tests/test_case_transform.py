4fdb4556661039fb802fd23821bccb86b8331661
Jack Werner <its.jack.werner@gmail.com>
2023-10-12 11:37:49 -0400

    added tests for missing column and for column not string type. Passed explicit schema to df fixture

--- tests/test_case_transform.py
+++ tests/test_case_transform.py
@@ -1,10 +1,13 @@
 import pytest
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StringType, StructField, DateType, IntegerType
+from pyspark.sql.utils import AnalysisException
 from collections import Counter
 from transforms.case_transform import case_transform
 
 COLUMN_NAME = 'name'
+CASE = "lowercase"
 
 @pytest.fixture(scope='module')
 def spark():
@@ -18,13 +21,25 @@ def spark():
 
 @pytest.fixture(scope='module')
 def accounts(spark):
-    df = spark.read.option('header','true').csv('tests/resources/accounts.csv')
+    schema = StructType([
+        StructField("id", IntegerType(), False),
+        StructField("name", StringType(), False),
+        StructField("created_date", DateType(), False),
+        StructField("state", StringType(), False),
+        StructField("sector", StringType(), False),
+    ])
+
+    df = (
+        spark.read
+        .option('header','true')
+        .schema(schema)
+        .csv('tests/resources/accounts.csv')
+    )
     yield df
     spark.stop()
 
 def test_transform_lowercase(accounts):
     df: DataFrame = accounts
-    case = 'lowercase'
     expected_values = [
         "abc company",
         "xyz corporation",
@@ -32,7 +47,7 @@ def test_transform_lowercase(accounts):
         "def industries"
     ]
 
-    df = case_transform.transform(df, COLUMN_NAME, case)
+    df = case_transform.transform(df, COLUMN_NAME, CASE)
     names_col = df.select(F.col(COLUMN_NAME)).collect()
     actual_values = [str(row[0]) for row in names_col]
 
@@ -63,9 +78,22 @@ def test_transform_bad_case(accounts):
     
     assert str(e.value) == "Provided value 'Lowercase' for parameter 'case' is invalid. Valid options are: uppercase, lowercase."
 
-def test_transform_missing_name(accounts):
-    pass
+def test_transform_missing_col(accounts):
+    df: DataFrame = accounts
+
+    with pytest.raises(ValueError) as e:
+        df = case_transform.transform(df, "Name", CASE)
+
+    assert str(e.value) == "Column 'Name' not found in DataFrame."
+
 
 def test_transform_not_string(accounts):
-    pass
+    df: DataFrame = accounts
+    
+    with pytest.raises(AnalysisException) as e:
+        df = case_transform.transform(df, "created_date", CASE)
+
+    assert str(e.value) == "Column 'created_date' is not of StringType."
+
+    
 
