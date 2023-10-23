a3458e643596554835e1434b737258355f290a06
Jack Werner <its.jack.werner@gmail.com>
2023-10-23 13:51:09 -0500

    added test that will fail to make sure workflow is working properly

--- tests/test_case_transform.py
+++ tests/test_case_transform.py
@@ -110,3 +110,7 @@ def test_transform_not_string(accounts):
         df = case_transform.transform(df, "created_date", CASE)
 
     assert str(e.value) == "Column 'created_date' is not of StringType."
+
+
+def test_false():
+    assert False
