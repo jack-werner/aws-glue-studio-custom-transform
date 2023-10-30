8f9c5caefb76de99d7931428e7c669e6b14d3569
Jack Werner <its.jack.werner@gmail.com>
2023-10-30 11:30:29 -0400

    changed all names back to their original values

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "custom_glue_studio_case_transform" {
   source     = "./tf_modules/custom_glue_studio_transform/"
   region     = "us-east-1"
   account_id = var.AWS_ACCOUNT_ID
-  filename   = "case_transform_string"
+  filename   = "case_transform"
   local_path = "transforms/case_transform"
 }
