50e922f24d3b0b83828c3970fdbea8d8f0f5b87c
Jack Werner <its.jack.werner@gmail.com>
2023-10-30 10:58:41 -0400

    updated filename

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "custom_glue_studio_case_transform" {
   source     = "./tf_modules/custom_glue_studio_transform/"
   region     = "us-east-1"
   account_id = var.AWS_ACCOUNT_ID
-  filename   = "case_transform"
+  filename   = "case_transform_string"
   local_path = "transforms/case_transform"
 }
